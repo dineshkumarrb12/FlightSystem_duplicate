36f54124a4290dede2869d1e43913ce5dc26903f
bhuvaneshmani <bhuvaneshmani@bhuvaneshs-mbp.attlocal.net>
2019-05-18 13:32:03 -0400

    updated cucumber runners

--- src/test/java/com/testng/learn/FlightBooking_duplicate/FlightLogin_TC.java
+++ src/test/java/com/testng/learn/FlightBooking_duplicate/FlightLogin_TC.java
@@ -2,12 +2,12 @@ package com.testng.learn.FlightBooking_duplicate;
 
 import org.openqa.selenium.WebDriver;
 
-import com.testng.learn.utilitest.DriverSupplices;
+import com.testng.learn.utilitest.DriverSupplier;
 
 public class FlightLogin_TC {
 	public WebDriver login()
 	{
-		DriverSupplices supplier=new DriverSupplices();
+		DriverSupplier supplier=new DriverSupplier();
 		WebDriver driver=supplier.driversetup();
 		FlightLogin logins=new FlightLogin(driver);
 		logins.login("dineshkumartest", "dineshtest");
